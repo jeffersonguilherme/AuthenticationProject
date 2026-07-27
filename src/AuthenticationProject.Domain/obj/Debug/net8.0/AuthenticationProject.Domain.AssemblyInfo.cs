507fccc1fa6162426b9a8bb26657276e7f0874fa
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-07-27 11:18:00 -0300

    Criado DomainException
    DomainException, criado para padronizar todas as resposta de erros.

--- src/AuthenticationProject.Domain/obj/Debug/net8.0/AuthenticationProject.Domain.AssemblyInfo.cs
+++ src/AuthenticationProject.Domain/obj/Debug/net8.0/AuthenticationProject.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenticationProject.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b92fe3ad2599c6f080e977c08c982fca6857934")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenticationProject.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenticationProject.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
